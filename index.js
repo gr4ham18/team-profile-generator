6d1fb1eb90d97e6fd0f8c4fc9e9da2bdbea9d939
graham <gr4ham18@gmail.com>
2022-01-19 20:34:51 -0500

    function to run app

--- index.js
+++ index.js
@@ -7,3 +7,42 @@ const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const html = require("./src/htmlTemp");
 const validator = require("email-validator");
+
+// async functions
+const writeFileAsync = util.promisify(fs.writeFile);
+const appendFileAsync = util.promisify(fs.appendFile);
+
+let teamArray = [];
+let teamString = ``;
+
+console.clear();
+console.log("---------------------------------------------");
+console.log("Team Portfolio Generator by Gr4ham18")
+
+// Fun to run app
+async function main() {
+    try {
+        await prompt()
+
+        for (let i = 0; i < teamArray.length; i++) {
+            teamString = teamString + html.generateCard(teamArray[i]);
+        }
+
+        let finalHtml = html.generateHTML(teamString)
+
+        console.clear();
+        console.log("---------------------------------------------");
+        console.log("Generating index.html file....");
+        console.log("---------------------------------------------");
+
+        writeFileAsync("./dist/index.html", finalHtml);
+
+        console.clear();
+        console.log("---------------------------------------------");
+        console.log("index.html file created successfully");
+        console.log("---------------------------------------------");
+
+    } catch (err) {
+        return console.log(err);
+    }
+}
