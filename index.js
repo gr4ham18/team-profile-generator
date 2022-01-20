160938ce15a4baee246e181105c50e88ea939906
graham <gr4ham18@gmail.com>
2022-01-19 20:34:02 -0500

    setting const

---
+++ index.js
@@ -0,0 +1,9 @@
+// const's
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const html = require("./src/htmlTemp");
+const validator = require("email-validator");
